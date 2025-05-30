0871531097978a8bd3715db6d80c048dfdfe3728
Maciej-Klobukowski <12401667@student.pxl.be>
2025-05-31 00:30:43 +0200

    Revert "fuck it broke"
    This reverts commit abb3875c00e9b3ca7af950926b689a80350121ca.

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
 const app = require('express')();
-const port = 8080;
+const port = 8080
 
-//req = incoming data res = outgoing data 
-app.get('/tshirt',(req,res) => {
-    res.status(200).send({
-        tshirt:'👕',
-        size:'large'
-    })
-    
-});
-
-
+app.listen(
+    port,
+    () => console.log('its alive on http://localhost:${port}')
+)
